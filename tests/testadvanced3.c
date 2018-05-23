fbb1487dd2d1ea9d7a879a71ad61358d84cf6a72
Arthur de Fluiter <arghhhhhthur@gmail.com>
2018-05-23 14:01:01 +0200

    extra comment for wide shit

--- tests/testadvanced3.c
+++ tests/testadvanced3.c
@@ -3,10 +3,28 @@
 #include "ijvm.h"
 #include "testutil.h"
 
+/*****************************************************************************
+ * Notes about wide:
+ *****************************************************************************
+ * WIDE modifies ILOAD, ISTORE, IINC making the local variables short rather
+ * than byte arguments.
+ *
+ * As mentioned in the method step(), a step should execute both WIDE and
+ * the next instruction in a single step.
+ ****************************************************************************/
+
+static void ignore_stdout(void)
+{
+    FILE *output = fopen("tmp_output", "w+");
+    assert(output);
+    set_output(output);
+}
+
 void test_wide1()
 {
     int res = init_ijvm("files/advanced/test-wide1.ijvm");
     assert(res != -1);
+    ignore_stdout();
 
     steps(6);
     assert(get_local_variable(1) == 0x1);
@@ -24,6 +42,7 @@ void test_wide2()
 {
     int res = init_ijvm("files/advanced/test-wide2.ijvm");
     assert(res != -1);
+    ignore_stdout();
 
     steps(6);
     assert(get_local_variable(1) == 0x1);
