91e914de27acfd40d6ff9e9385a5a87f25b5aa64
Stef van Schie <stef@van-schie.eu>
2022-12-10 16:03:44 +0000

    Include all required functions by default

--- src/machine.c
+++ src/machine.c
@@ -25,3 +25,69 @@ void set_output(FILE *fp)
 {
   // TODO: implement me
 }
+
+byte_t *get_text()
+{
+  // TODO: implement me
+  return NULL;
+}
+
+int text_size()
+{
+  // TODO: implement me
+  return 0;
+}
+
+int get_program_counter()
+{
+  // TODO: implement me
+  return 0;
+}
+
+byte_t get_instruction()
+{
+  //TODO: implement me
+  return 0;
+}
+
+word_t tos()
+{
+  // TODO: implement me
+  return 0;
+}
+
+word_t *get_stack()
+{
+  // TODO: implement me
+  return NULL;
+}
+
+int stack_size()
+{
+  // TODO: implement me
+  return 0;
+}
+
+bool step()
+{
+  // TODO: implement me
+  return false;
+}
+
+bool finished()
+{
+  // TODO: implement me
+  return false;
+}
+
+word_t get_local_variable(int i)
+{
+  // TODO: implement me
+  return 0;
+}
+
+word_t get_constant(int i)
+{
+  // TODO: implement me
+  return 0;
+}
