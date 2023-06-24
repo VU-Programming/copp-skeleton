2c7786fa58a2ff5bffbbb0b3bd28e772a4e76c91
GregorySwiader <g.k.swiader@student.vu.nl>
2023-06-24 12:37:46 +0200

    Fixed the formatting of the new file

--- src/intercept.c
+++ src/intercept.c
@@ -1,20 +1,20 @@
 #include "intercept.h"
 
-// #define LOG_EXIT()
-
 typedef void (*exit_handle)(int);
 
-void orig_exit(int status) {
-    static exit_handle _orig_exit;
-    if (!_orig_exit)
-        _orig_exit = (exit_handle)dlsym(RTLD_NEXT, "exit");
-    _orig_exit(status);
-    abort(); // will never be called, used to suppress a compiler warning!
+void orig_exit(int status)
+{
+  static exit_handle _orig_exit;
+  if (!_orig_exit)
+    _orig_exit = (exit_handle)dlsym(RTLD_NEXT, "exit");
+  _orig_exit(status);
+  abort(); // will never be called, used to suppress a compiler warning!
 }
 
-void exit(int status) {
-    status--; // to suppress pedantic warnings
-    fprintf(stderr, "Do not use exit in the basic functionalities."
-                    " You won't be graded correctly if you do!\n");
-    orig_exit(1);
+void exit(int status)
+{
+  status--; // to suppress pedantic warnings
+  fprintf(stderr, "Do not use exit in the basic functionalities."
+                  " You won't be graded correctly if you do!\n");
+  orig_exit(1);
 }
