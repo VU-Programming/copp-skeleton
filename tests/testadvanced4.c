dc3f1c945c31faa3775ca1fc66f244c6d55cae2a
Atze <atzeus@gmail.com>
2024-05-06 17:01:01 +0200

    Closed wrong thing

--- tests/testadvanced4.c
+++ tests/testadvanced4.c
@@ -28,7 +28,7 @@ void run_calc_inp(char *input, char *expected)
     assert(strncmp(buf, expected, strlen(expected)) == 0);
 
     destroy_ijvm(m);
-    fclose(input);
+    fclose(inputf);
     fclose(out_file);
 }
 
